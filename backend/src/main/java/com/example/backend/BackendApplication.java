84512def9b3acaa47bc736b5be10606f5d5b5652
draganjovic96 <sasailickapiten22@gmail.com>
2019-12-18 16:15:31 +0100

    Added backend and frontend project and updated database model.

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.example.ISA;
+package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IsaApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IsaApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.ISA;
+package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IsaApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
